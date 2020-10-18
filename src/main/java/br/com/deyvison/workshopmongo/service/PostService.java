9ceb5c3fee0ce8278f70195f7c19b295e63355a0
deyvison uchoa <deyvisonuchoa@hotmail.com>
2020-10-18 14:56:01 -0300

    Consulta com vários critérios

--- src/main/java/br/com/deyvison/workshopmongo/service/PostService.java
+++ src/main/java/br/com/deyvison/workshopmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package br.com.deyvison.workshopmongo.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,5 +28,10 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text){
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000); //adicionar um dia após a data final
+		return repo.fullSerachWithDate(text, minDate, maxDate);
 	}
 }
